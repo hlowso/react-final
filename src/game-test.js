ebe14525a943390b4caf25c2036b6a7e2bbdb748
hlowso <h.canderleigh@gmail.com>
2018-04-08 17:05:37 +0000

    text appears to be appearing

---
+++ src/game-test.js
@@ -0,0 +1,145 @@
+import generateCode from "./code-generator.js";
+// import PidgeonIcon from "./assets/pigeon_ph.png";
+// import SkyBackground from "./assets/sky.png";
+
+function start() {
+  const gameAttributes = {
+    code: "buster", //generateCode(),
+    spriteSize: 40,
+    gameWidth: window.innerWidth * window.devicePixelRatio,
+    gameHeight: window.innerHeight * window.devicePixelRatio,
+    gameSpeed: 100
+  };
+
+  window.onload = function() {
+    const gameConfig = {
+      type: Phaser.AUTO,
+      width: gameAttributes.gameWidth,
+      height: gameAttributes.gameHeight,
+      physics: {
+        default: "arcade",
+        arcade: {
+          // gravity: { y: 20 }
+        }
+      },
+      scene: [playGame]
+    };
+
+    const game = new Phaser.Game(gameConfig);
+    // resize();
+    // window.addEventListener("resize", resize, false);
+  };
+
+  let player;
+  let cursors;
+  let x_velocity = 0.0;
+  let y_velocity = 0.0;
+  let shooting = false;
+  let playerScore;
+  let score = 0;
+
+  const playGame = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize: function playGame() {
+      Phaser.Scene.call(this, { key: "PlayGame" });
+    },
+
+    preload: function() {
+      this.load.image("background", "cb8fe21064e648d4fd4aa71e2a5d339d.png"); //SkyBackground);
+      this.load.image(
+        "pigeon",
+        "./684f058312efc4690e0eac66c3b3930e.png",
+        129,
+        84
+      ); //PidgeonIcon, 129, 84);
+    },
+
+    create: function() {
+      // TEMPORARY PLACEMENT FOR WS
+
+      const ws = new WebSocket(window.location.origin.replace(/^http/, "ws"));
+      ws.onopen = () => {
+        ws.send(
+          JSON.stringify({
+            device: "desktop",
+            code: gameAttributes.code
+          })
+        );
+      };
+      ws.onmessage = incoming_message => {
+        const message = JSON.parse(incoming_message.data);
+        switch (message.subject) {
+          case "push":
+            x_velocity = velocity.x;
+            y_velocity = velocity.y;
+            break;
+          case "shoot":
+            shooting = message.shooting;
+            break;
+        }
+      };
+      // ---------------
+
+      const background = this.add.image(
+        gameAttributes.gameWidth / 2,
+        gameAttributes.gameHeight / 2,
+        "background"
+      );
+
+      background.setScale(window.devicePixelRatio * 2);
+
+      player = this.physics.add.sprite(
+        gameAttributes.gameWidth / 2,
+        gameAttributes.gameHeight / 2,
+        "pigeon"
+      );
+      // let fly = player.animations.add('right', [0,1,2,3,4,5]);
+      player.setBounce(0.4);
+      player.setCollideWorldBounds(true);
+
+      playerScore = this.add.text(100, 100, `${score}`);
+      this.add.text(200, 200, `Code: ${gameAttributes.code}`);
+
+      cursors = this.input.keyboard.createCursorKeys();
+    },
+
+    update: function() {
+      // code_message.setText(`Code: ${gameAttributes.code}`);
+
+      if (y_velocity > 0) {
+        player.rotation = Math.atan(x_velocity / y_velocity);
+      } else if (y_velocity === 0.0) {
+        if (x_velocity < 0) {
+          player.rotation = 0.5 * Math.PI;
+        } else {
+          player.rotation = 1.5 * Math.PI;
+        }
+      } else {
+        player.rotation = Math.atan(x_velocity / y_velocity) + Math.PI;
+      }
+      player.setVelocityX(250.0 * x_velocity);
+      player.setVelocityY(-250.0 * y_velocity);
+
+      if (shooting) {
+        console.log("I'm SHOOTING!");
+      }
+    }
+  });
+
+  function resize() {
+    const canvas = document.querySelector("canvas");
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const windowRatio = windowWidth / windowHeight;
+    const gameRatio = game.config.width / game.config.height;
+    if (windowRatio < gameRatio) {
+      canvas.style.width = windowWidth + "px";
+      canvas.style.height = windowWidth / gameRatio + "px";
+    } else {
+      canvas.style.width = windowHeight * gameRatio + "px";
+      canvas.style.height = windowHeight + "px";
+    }
+  }
+}
+
+export default start;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import start from "./game.js";
+import start from "./game-test.js";
+// import start from "./game.js";
 import App from "./App.js";
 import MobileApp from "./MobileApp.js";
 
 const isMobile = () => {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  );
+	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+		navigator.userAgent
+	);
 };
 
 if (isMobile()) {
-  ReactDOM.render(<MobileApp />, document.getElementById("root"));
+	ReactDOM.render(<MobileApp />, document.getElementById("root"));
 } else {
-  start();
+	start();
 }
