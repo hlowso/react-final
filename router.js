cf02100b24cd5b28867d2753e681fd63e795dc65
hlowso <h.canderleigh@gmail.com>
2018-04-12 01:33:03 +0000

    basically set up ALL the backend we're gunna need

---
+++ router.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+module.exports = db => {
+  const router = express.Router();
+  router.use(bodyParser.urlencoded({ extended: false }));
+
+  const userScoresCollection = db.collection("userScores");
+  const teamScoresCollection = db.collection("teamScores");
+
+  router.get("/", (request, response) =>
+    response.sendFile(__dirname + "/dist/index.html")
+  );
+
+  router.get("/user-scores", (request, response) => {
+    userScoresCollection.find().toArray((err, scores) => {
+      if (err) {
+        console.log("There was an error retrieving user scores");
+        response.status(500);
+      }
+      scores.sort((a, b) => b.killCount - a.killCount);
+      response.send(scores);
+    });
+  });
+
+  router.post("/user-scores", (request, response) => {
+    const { username, killCount } = request.body;
+    userScoresCollection.insertOne(
+      {
+        username,
+        killCount
+      },
+      (err, result) => {
+        if (err) {
+          console.log("There was an error adding a user score");
+          response.status(500);
+        }
+        response.send(result);
+      }
+    );
+  });
+
+  router.get("/team-scores", (request, response) => {
+    teamScoresCollection.find().toArray((err, scores) => {
+      if (err) {
+        console.log("There was an error retrieving team scores");
+        response.status(500);
+      }
+      scores.sort((a, b) => b.score - a.score);
+      response.send(scores);
+    });
+  });
+
+  router.post("/team-scores", (request, response) => {
+    const { teamname, score, totalKills } = request.body;
+    teamScoresCollection.insertOne(
+      {
+        teamname,
+        score,
+        totalKills
+      },
+      (err, result) => {
+        if (err) {
+          console.log("There was an error adding a team score");
+          response.status(500);
+        }
+        response.send(result);
+      }
+    );
+  });
+
+  return router;
+};
--- server.js
+++ server.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 
-module.exports = db => {
-  const router = express.Router();
-  router.get("/", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
-  router.get("/leaderboard", (req, res) => {
-    // TODO
+module.exports = routes => {
+	const server = express()
+		.use(express.static("dist"))
+		.use("/", routes)
+		.listen(PORT, () =>
+			console.log(`Express server listening on port ${PORT}`)
+		);
 
-    console.log("will do stuff to satisfy this get request");
-    res.send();
-  });
-  router.put("/leaderboard", (req, res) => {
-    // TODO
-
-    console.log("will do stuff to satisfy this put request");
-  });
-
-  const server = express()
-    .use(express.static("dist"))
-    .use("/", router)
-    .listen(PORT, () =>
-      console.log(`Express server listening on port ${PORT}`)
-    );
-
-  require("./websocket-server.js")(server);
+	require("./websocket-server.js")(server);
 };
