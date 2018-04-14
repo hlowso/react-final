68e0961192778ed9dd51f8038b2a2376c7c2b70a
hlowso <h.canderleigh@gmail.com>
2018-04-14 21:39:45 +0000

    nullified onmessage function to try to improve over-heating issue

--- src/play-scene-sequences/update.js
+++ src/play-scene-sequences/update.js
@@ -2,18 +2,24 @@ export default function() {
 	this.vars.score += 1;
 	this.vars.gameScoreText.setText("Score: " + this.vars.score);
 
-	let game_over = true;
-	for (let player_id in this.entities.players.individuals) {
-		let player = this.entities.players.individuals[player_id];
+	if (this.vars.player_ids.length) {
+		let game_over = true;
+		for (let player_id in this.entities.players.individuals) {
+			let player = this.entities.players.individuals[player_id];
 
-		if (player.alive) {
-			game_over = false;
-			if (player.shooting) {
-				this.fireBullet(player);
+			if (player.alive) {
+				game_over = false;
+				if (player.shooting) {
+					this.fireBullet(player);
+				}
 			}
 		}
-	}
-	if (game_over) {
-		this.scene.start("End", { vars: this.vars, entities: this.entities });
+		if (game_over) {
+			this.vars.ws.onmessage = null;
+			this.scene.start("End", { vars: this.vars, entities: this.entities });
+		}
+	} else {
+		this.vars.ws.onmessage = null;
+		this.scene.start("Lobby", { vars: this.vars });
 	}
 }
